792b75e43a9579114935710b6bd68ea9ff65a362
George Mihailov <gm@inlead.dk>
2013-07-10 12:16:22 +0300

    case 3003 - Add context for update handling.

--- js/ting_search.js
+++ js/ting_search.js
@@ -4,24 +4,19 @@
     // Handle async material status update.
     $(document).bind('materials_status', function(e, data) {
       $.each(data, function(i, e) {
+        var $context = $('#availability-' + e.local_id).closest('.search-result');
+
         // Reservation.
-        var $reservation_controls = $('.btn.reservation'); // .reservation !!
-        if (e.show_reservation_button) {
-          $reservation_controls.show();
-        }
-        else {
+        var $reservation_controls = $('.btn.reservation', $context);
+        if (!e.show_reservation_button) {
           $reservation_controls.remove();
         }
 
         // Holdings.
-        var $holdings_controls = $('.btn.find-it-here, .group_holdings');
-        var $sticky_menu_item = $('.goto-' + $($holdings_controls[1]).attr('id'));
+        var $holdings_controls = $('.btn.find-it-here, .group_holdings', $context);
+        var $sticky_menu_item = $('.goto-' + $($holdings_controls[1]).attr('id'), $context);
 
-        if (e.holdings_available.length > 0) {
-          $holdings_controls.show();
-          $sticky_menu_item.show();
-        }
-        else {
+        if (e.holdings_available.length == 0) {
           $holdings_controls.remove();
           $sticky_menu_item.remove();
         }
